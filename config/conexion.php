be2e2d4c60e912705b6525eb3a3a189da38618a1
Marlon Farias <fmarlonleonardo@gmail.com>
2025-08-14 19:21:18 -0300

    i

--- config/conexion.php
+++ config/conexion.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< Updated upstream
 
 $servername = "localhost";
 $username = "root";
@@ -11,4 +12,20 @@ if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
 }
 
+=======
+function conectar() {
+    $host = 'localhost';
+    $usuario = 'root';
+    $clave = '';
+    $base_datos = 'basedatos';
+
+    $conexion = new mysqli($host, $usuario, $clave, $base_datos);
+
+    if ($conexion->connect_error) {
+        die('Error de conexión: ' . $conexion->connect_error);
+    }
+
+    return $conexion;
+}
+>>>>>>> Stashed changes
 ?>
